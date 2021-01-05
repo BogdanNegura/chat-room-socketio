9021ec58cbf55ddf8e9a4518e82778e8908d6513
Bogdan Negura <id.bogdann@yahoo.com>
2021-01-05 22:09:42 +0000

    create the sidebar using bootstrap

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import { SideBar } from "../sidebar/sidebar.component"
 
 const Dashboard = ({ id }) => {
-    return (
-        <div>
-            {id}
-        </div>
-    )
+  return (
+    <div>
+      <SideBar id={id}/>
+    </div>
+  )
 }
 
 export { Dashboard }
