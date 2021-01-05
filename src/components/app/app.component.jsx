827488a96ce786c5c1c728f0731c63e45699537e
Bogdan Negura <id.bogdann@yahoo.com>
2021-01-05 21:49:30 +0000

    print dashboard when an id its created

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Dashboard } from "../dashboard";
 import { UseLocalStorage } from "../hooks/use-local-storage";
 import { Login } from "../login";
 
@@ -7,11 +8,7 @@ const App = () => {
   const [id, setId] = UseLocalStorage()
 
   return (
-    <>
-      {id}
-      <Login onIdSubmit={setId}/>
-  
-    </>
+      id ? <Dashboard id={id}/> : <Login onIdSubmit={setId}/>
   )
 }
 
